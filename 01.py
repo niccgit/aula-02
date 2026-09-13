2d7e71fa79ab217750b2f93492aae054db8e1af1
Nicolly J. Lima <nicollylima747@outlook.com.br>
2026-09-13 19:21:32 -0300

    atualização na estrutura do código
    código 01.py - aula 02 de lopal

--- 01.py
+++ 01.py
@@ -1,25 +1,34 @@
-# Escreva um programa em pythonque peça as seguintes informacoes:
-# idade (numero inteiro)
-# altura em centimetros ( numero inteiro)
-# autorizacao dos pais (sim ou nao)
+# Escreva um programa em Python que peça as seguintes três informações do usuário:
 #
-# o programa deve exibir "acesso liberado", se o visitante puder andar no brinquedo
-# o programa devev exibir "acesso negado", caso contrario
+# Idade do visitante (int)
+# Altura do visitante (int) - em centímetros 
+# Autorização dos pais (str) - S ou N
 #
-# AS REGRAS SAO:
-# ter idade >= 12 anos de idade
-# !!!! E !!!!!
-# ter altura >= 140 de altura
+# O programa deve exibir "Acesso liberado!", se o visitante atender às regras de uso dos brinquedos
+#
+# CASO CONTRÁRIO 
+#
+# O programa deve exibir "Acesso negado.", se o visitante não atender às regras de uso dos brinquedos
+#
+# AS REGRAS SÃO:
+#
+# Ter idade maior ou igual à 12 anos de idade E Ter altura maior ou igual à 140 centímetros 
 # !!!! OU !!!!
-# se tiver a autorizacao dos pais
+# Ter a Autorização dos pais
 
-idade = int(input ("Olá! Digite a sua idade: "))
-altura = int(input ("Agora digite a sua altura (em centímetros): "))
-autorizacao_dos_pais = input("Você tem a autorização dos seus pais?")
 
-autorizacao_dos_pais = "sim"
+idade_visitante = int(input("Qual é a sua idade?"))
+altura_visitante = int(input("Qual é a sua altura (em centímetros)?"))
+autorizacao = input("Você tem a autorização dos seus pais? Responda com 'Sim' ou 'Não'").lower()
 
-if idade >= 12 and altura >= 140 or autorizacao_dos_pais == "sim":
+opcoes_autorizacao = ["sim", "não", "nao"]
+
+
+while autorizacao not in opcoes_autorizacao:
+    autorizacao = input("Resposta inválida! Responda somente com 'Sim' ou 'Não'").lower()
+
+
+if idade_visitante >= 12 and altura_visitante >= 140 or autorizacao == "sim": 
     print("Acesso liberado!")
 else:
     print("Acesso negado.")
