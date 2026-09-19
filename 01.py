9d678587995b5fa1ed24c05e1d1e1161b770c212
Nicolly J. Lima <nicollylima747@outlook.com.br>
2026-09-18 23:25:05 -0300

    atualização *final na estrutura do código
    01_entrada.py

--- 01.py
+++ 01.py
@@ -1,34 +1,36 @@
 # Escreva um programa em Python que peça as seguintes três informações do usuário:
 #
 # Idade do visitante (int)
-# Altura do visitante (int) - em centímetros 
+# Altura do visitante (int) - em centímetros
 # Autorização dos pais (str) - S ou N
 #
 # O programa deve exibir "Acesso liberado!", se o visitante atender às regras de uso dos brinquedos
 #
-# CASO CONTRÁRIO 
+# CASO CONTRÁRIO
 #
 # O programa deve exibir "Acesso negado.", se o visitante não atender às regras de uso dos brinquedos
 #
 # AS REGRAS SÃO:
 #
-# Ter idade maior ou igual à 12 anos de idade E Ter altura maior ou igual à 140 centímetros 
+# Ter idade maior ou igual à 12 anos de idade E Ter altura maior ou igual à 140 centímetros
 # !!!! OU !!!!
 # Ter a Autorização dos pais
 
 
-idade_visitante = int(input("Qual é a sua idade?"))
-altura_visitante = int(input("Qual é a sua altura (em centímetros)?"))
-autorizacao = input("Você tem a autorização dos seus pais? Responda com 'Sim' ou 'Não'").lower()
-
 opcoes_autorizacao = ["sim", "não", "nao"]
 
 
+idade_visitante = int(input("Qual é a sua idade?"))
+
+altura_visitante = int(input("Qual é a sua altura (em centímetros)?"))
+
+autorizacao = input("Você tem a autorização dos seus pais? Responda com 'Sim' ou 'Não'").lower()
+
 while autorizacao not in opcoes_autorizacao:
-    autorizacao = input("Resposta inválida! Responda somente com 'Sim' ou 'Não'").lower()
+    autorizacao = input("Resposta inválida! Responda somente com 'Sim' ou 'Não': ").lower()
 
 
-if idade_visitante >= 12 and altura_visitante >= 140 or autorizacao == "sim": 
+if (idade_visitante >= 12 and altura_visitante >= 140) or (autorizacao == "sim"):
     print("Acesso liberado!")
 else:
     print("Acesso negado.")
